2e0718b0a414d671560008c99ee2e2e3000c5586
asylum2010 <darthasylum@hotmail.com>
2012-03-07 10:09:36 +0100

    ok now i guess i really fixed the vc100 projs (except the dx10 one)

--- projects/source/emptyproject10/main.cpp
+++ projects/source/emptyproject10/main.cpp
@@ -1,21 +1,21 @@
 //*************************************************************************************************************
 #include "emptyproject10.h"
 
-#pragma comment(lib, "dxguid.lib")
-#pragma comment(lib, "dinput8.lib")
+#pragma comment(lib, "../../../extern/dxguid.lib")
+#pragma comment(lib, "../../../extern/dinput8.lib")
 
 #ifdef _DEBUG
 #	pragma comment(lib, "../../../code/lib/Debug/DummyFramework.lib")
-#   pragma comment(lib, "d3d10.lib")
-#   pragma comment(lib, "d3dx10.lib") // d3dx10d.lib
+#   pragma comment(lib, "../../../extern/d3d10.lib")
+#   pragma comment(lib, "../../../extern/d3dx10.lib") // d3dx10d.lib
  
 #   define _CRTDBG_MAP_ALLOC
 #   include <stdlib.h>
 #   include <crtdbg.h>
 #else
 #   pragma comment(lib, "../../../code/lib/Release/DummyFramework.lib")
-#   pragma comment(lib, "d3d10.lib")
-#   pragma comment(lib, "d3dx10.lib")
+#   pragma comment(lib, "../../../extern/d3d10.lib")
+#   pragma comment(lib, "../../../extern/d3dx10.lib")
 #endif
 
 int main()
